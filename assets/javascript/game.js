6c04e9247335b0d1c605dbf1a0415375b76e8a6b
Dan MItchell <danmitchell@Dans-MacBook-Pro.local>
2019-04-12 11:32:29 -0400

    starting JV

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+
+// generate rand om number from 19-120
+document.getElementById("randomNumber").innerHTML
+Math.floor(Math.random() * 120) + 19
+console.log(randomNumber)
+
+
+// on click generate rand random number 1-12
+Math.floor(Math.random() * 12) + 1
+// if number adds up to total random you win
+
+// reset once number matched is equal
+// count wins and loses
+
